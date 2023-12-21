8d0f711800001537d837fad1faef7a48ee4ddc9c
Vitalii <svitalii138@gmail.com>
2023-12-21 23:23:51 +0200

    server added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
-app.get('/', (req, res) => {
- res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
- console.log('Server is run');
+  console.log("Server is run");
 });
---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require("./app");
+
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
