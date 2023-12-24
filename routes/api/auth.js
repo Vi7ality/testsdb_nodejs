1ae0431605754b011c1f9b122d3dad4b8f41a7fc
Vitalii <svitalii138@gmail.com>
2023-12-24 23:58:03 +0200

    auth route added

---
+++ routes/api/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const ctrl = require("../../controllers/auth");
+const { authenticate } = require("../../middlewares");
+
+router.post("/register", ctrl.register);
+router.post("/login", ctrl.login);
+router.get("/current", authenticate, ctrl.getCurrent);
+router.post("/logout", authenticate, ctrl.logout);
+
+module.exports = router;
