19679490c45b495ea04f338f00d5adc0978ea1d7
Vitalii <svitalii138@gmail.com>
2023-12-25 01:49:39 +0200

    login router added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,7 @@ const ctrl = require("../../controllers/auth");
 // const { authenticate } = require("../../middlewares");
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
-// router.post("/login", ctrl.login);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 // router.get("/current", authenticate, ctrl.getCurrent);
 // router.post("/logout", authenticate, ctrl.logout);
 
