42c36514246c23496c860b04cebda127e56ad4db
Vitalii <svitalii138@gmail.com>
2023-12-24 23:56:59 +0200

    user model added

---
+++ models/user.js
@@ -0,0 +1,57 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers/index");
+
+const emailRegexp = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    token: { type: String, default: "" },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .email()
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
